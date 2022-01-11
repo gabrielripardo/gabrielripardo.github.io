23bdedaa21db9941c55408755829a72f05369661
Gabriel <gts.senna@gmail.com>
2022-01-11 00:16:04 -0300

    portfolio com sidebar

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarMensagem(){
+    window.alert("IMG: tocando piano")
+}    
+
+window.onload = function(){
+    const date = new Date()
+    document.getElementById("current-year").innerText = date.getFullYear()    
+}
+
+  
