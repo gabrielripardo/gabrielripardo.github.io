785979b9d74c7a3a70c3fe268c70b168a933e8f4
gabrielripardo <gts.senna@gmail.com>
2022-04-19 01:01:40 -0300

    test new heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">to my website</a>
+          Welcome to <a href="https://nextjs.org"> my website</a>
         </h1>
 
         <p className={styles.description}>
